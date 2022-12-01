f419496c8960b969db68f6169c58b4a06d9e1c81
NarwhalPotato <112595401+NarwhalPotato@users.noreply.github.com>
2022-12-01 09:41:49 -0600

    Update User.java

--- CS321_Group4_Project/src/User.java
+++ CS321_Group4_Project/src/User.java
@@ -1,2 +1,25 @@
+//information that should be stored in the user
+// class identifier - time start - time end - location
 public class User {
+    // **Q: do we need an option for day of the week?
+    String Identifier;
+    int timeStart;
+    int timeEnd;
+    String location;
+
+    //Empty Constructor
+    public User() {
+
+    }
+
+    //User Constructor with all inputs
+    public Student(String Id, int start, int end, String loc){
+        Identifier = Id;
+        timeStart = start;
+        timeEnd = end;
+        location = loc;
+    }
+
+    //will need functions to return certain values from the class
+
 }
