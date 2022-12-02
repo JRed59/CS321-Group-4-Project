b83a2badd1846da6ca7ae2a7267a032e185cae24
NarwhalPotato <112595401+NarwhalPotato@users.noreply.github.com>
2022-12-01 21:45:53 -0600

    Update DataBaseScanner.java

--- CS321_Group4_Project/src/DataBaseScanner.java
+++ CS321_Group4_Project/src/DataBaseScanner.java
@@ -1,2 +1,13 @@
+import java.io.*;  
+
 public class DataBaseScanner {
+    public DataBaseScanner() {
+        try {
+            File filename = newFile("UAHDining.txt");
+            FileInputStream fis = new FileInputStream(filename);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
